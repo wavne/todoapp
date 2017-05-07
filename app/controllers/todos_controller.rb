6749d09b0e05c8d51ae9438782148e406917b6ad
Wayne Tooley <iwavne@gmail.com>
2017-05-07 03:27:18 +0000

    Add delete section

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -36,6 +36,12 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
   
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] = "Todowas deleted successfully"
+    redirect_to todos_path
+  end
   
   private
   
